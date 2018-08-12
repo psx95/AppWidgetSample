d0d36d8034e66b18073deee3598dd70a5a3c8369
psx95 <pranav.ps95@hotmail.com>
2018-08-12 23:05:33 +0530

    feat(WidgetUpdate): Updates the widget layout, and implements the onUpdate

--- app/src/main/java/com/psx/appwidgetsample/NewAppWidget.java
+++ app/src/main/java/com/psx/appwidgetsample/NewAppWidget.java
@@ -3,25 +3,38 @@ package com.psx.appwidgetsample;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
 
+    private static final String sharedPrefFile = "com.psx.app_widget";
+    private static final String COUNT_KEY = "count";
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        SharedPreferences sharedPreferences = context.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE);
+        int count = sharedPreferences.getInt(COUNT_KEY + appWidgetId, 0);
+        count++;
+        String dateStr = DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.app_widget_id, widgetText);
-
+        views.setTextViewText(R.id.app_widget_id, appWidgetId+"");
+        views.setTextViewText(R.id.update_date, context.getResources().getString(R.string.date_count_format, count, dateStr));
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(COUNT_KEY + appWidgetId, count);
+        editor.apply();
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
-
+    
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
